873ac79fe501456b315b0c990bd5281546148643
tonypadawan <anthonybruno534@gmail.com>
2022-08-02 21:10:35 -0300

    Added: Random number, levels and points.

--- adivinhando.py
+++ adivinhando.py
@@ -1,11 +1,40 @@
+import random
+import os
+
+
 # variables
-secret = 38
-lifes = 3
+secret_number = random.randint(1, 100)
 attempts = 0  # Tentativas
+lifes = 0
+points = 500
+test_level = 1
+
 
 print('+-' * 17)
 print(' Welcome ao jogo da Adivinhação!')
-print('+-' * 17)
+print('+-' * 17 )
+print()
+
+while test_level == 1:
+    level = int(input('Escolha a dificuldade: [ 1 - Fácil, 2 - Normal, 3 - Difícil ] : '))
+
+    if level < 1 or level > 3:
+        print('Valor invalido! Escolha entre 1 e 3. \n')
+
+    elif level == 1:
+        lifes = 12
+        test_level = 0
+
+    elif level == 2:
+        lifes = 8
+        test_level = 0
+
+    else:
+        lifes = 5
+        test_level = 0
+
+os.system('clt' if os.name == 'nt' else 'clear')
+
 
 for attempts in range(1, lifes + 1):
 
@@ -24,17 +53,34 @@ for attempts in range(1, lifes + 1):
 
     print()
 
-    hit = kick_number == secret  # Acertou
-    less = kick_number < secret  # Menor
+    hit = kick_number == secret_number  # Acertou
+    less = kick_number < secret_number  # Menor
 
+    # Limpa a tela;
+    os.system('cls' if os.name == 'nt' else 'clear') 
+    
+    
     if hit:
-        print('Parabéns, Você acertou! \n')
         break
 
     elif less:
         print(f'Você errou! {kick_number} é MENOR que o número secreto! \n')
+        points -= (secret_number - kick_number) / 2
 
     else:
         print(f'Você errou! {kick_number} é MAIOR que o número secreto! \n')
+        points -= (kick_number - secret_number) / 2
+    
+    print()
 
+
+if hit:
+    print(f'Parabéns, Você acertou! O número secreto era {secret_number}.')
+
+else:
+    print(f'Que pena, você perdeu! O número secreto era {secret_number}.')
+    print('Tente novamente! \n')
+
+
+print(f'Você fez {points} pontos! \n')
 print('Game-Over')
